ddba832bd0fb5dab84b8f9185670ccf7376ece48
onthefly4445 <martin.nastoupil03@gmail.com>
2024-03-20 17:29:31 +0200

    very basic scene transtions

--- Assets/Scripts/ClickableObject.cs
+++ Assets/Scripts/ClickableObject.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClickableObject : MonoBehaviour
 {
     public string objectName;
     public Vector2 nameTagSize;
+    public string sceneToLoad = "";
+
+    public void changeScence() {
+        if (sceneToLoad != "") {
+            SceneManager.LoadSceneAsync(sceneToLoad); 
+        }
+    }
+
 }
